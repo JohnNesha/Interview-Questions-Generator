dac27d4320b6bcd9bd4d1e63d8f04836e297cc1f
johnneshagraves <johnneshagraves@gmail.com>
2025-06-14 20:18:58 -0400

    Added API to provide question

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-function generateQuestion(event) {
-  event.preventDefault();
-
+function displayQuestion(response) {
   new Typewriter("#question_type", {
-    strings: "Question goes here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
   });
+}
+
+function generateQuestion(event) {
+  event.preventDefault();
+
+  let careerInput = document.querySelector("#career_question");
+  let apiKey = "a723fd412o41a9d1a23tfcb7443f0307";
+  let context =
+    "You are an AI assistant that helps individuals with common interview questions for any career. ";
+  let questionPrompt = `Supply an interview question based on ${careerInput.value} career field and then provide only 2  simple hints on how the question should be answered after supplying the question. Separate interview question from hints.`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${questionPrompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayQuestion);
 }
 
 let questionFormElement = document.querySelector("#question_generator_form");
